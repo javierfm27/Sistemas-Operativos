9714ccef5935403f55ddd98d2579dedccfe7339d
javierfm27 <javitofm27@github.com>
2017-10-19 19:56:06 +0200

    E5 actualizado

--- E5/ccall.c
+++ E5/ccall.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <err.h>
 
 int
 main (int argc, char * argv[]){
   printf("PROGRAMA CCALL\n===============\n\n");
-  
+
+  if(argc > 2){
+    errx(1,"Too much arguments.Example: ./ccall [path]");
+  }else{
+    if(argc == 1){
+      printf("El directorio actual\n");
+    }else{
+      printf("El directorio por argumento\n");
+    }
+    exit(0);
+  }
 }
