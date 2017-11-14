837b71b1f4abf2f9bbf7e9210c050d37b7ef5c05
javierfm27 <javitofm27@github.com>
2017-11-15 00:00:14 +0100

    Inacabado

--- E7/ccall.sh
+++ E7/ccall.sh
@@ -1,12 +1,50 @@
 #!/bin/sh
 
-shift 1
-echo $#
+ficherosC=*.c
+dflag=n
 cflag=n
 wordflag=n
-dirflag=n
-checkarguments()
-{
-  case $# in
-    1) echo hola;;
-}
+echo $#
+if test $# -eq 0
+then
+  for i in $ficherosC
+  do
+    gcc -c $i
+  done
+else
+  for i in $@
+  do
+    echo bucle $i
+    case $i in
+      *-d*)
+        echo entra en d
+        dflag=y
+        shift
+        echo despues de asignar $#
+        directorio=$1;;
+      *-c*)
+        echo entra en c
+        cflag=y
+        shift
+        options=$1;;
+      *)
+        wordflag=y
+        word=$1;;
+    esac
+    if test $# -eq 1
+    then
+      break
+    else
+      shift
+    fi
+  done
+fi
+echo $directorio
+echo $word
+echo $options
+#Ahora hacemos las compilaciones
+if test [$cflag = y] && [$dflag = y]
+then
+  echo si
+fi
+exit 0
