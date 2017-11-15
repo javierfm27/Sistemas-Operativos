b334ad7ef2f33d1483371550d7f4412a5ba39735
javierfm27 <javitofm27@github.com>
2017-11-15 23:52:20 +0100

    Inacabado

--- E8/makemarks.sh
+++ E8/makemarks.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo hola
+
+#Lo primero es comprobar que nos pasan como argumentos 4 ficheros
+if test $# -eq 4
+then
+  touch notasfinales.txt
+else
+  echo usage error: $0 [file1.txt] [file2.txt] [file3.txt] [file4.txt]
+fi
---
+++ E8/pruebaMakeMarks.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./makemarks.sh notas1.txt notas2.txt notas3.txt notas4.txt
