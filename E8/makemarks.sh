fbe6b61bfcb707013bd5431abeca43b0b1a8eb12
javierfm27 <javitofm27@github.com>
2017-11-20 10:29:53 +0100

    Inacabado

--- E8/makemarks.sh
+++ E8/makemarks.sh
@@ -4,6 +4,17 @@
 if test $# -eq 4
 then
   touch notasfinales.txt
+  echo "#Nombre   Ejer1   Ejer2   Ejer3   Ejer4   Final" >notasfinales.txt
 else
   echo usage error: $0 [file1.txt] [file2.txt] [file3.txt] [file4.txt]
+  exit 1
 fi
+#Nos quedamos con todos los alumnos, en una variable para luego asi saber quien no realizo la entrega de algun ejercicio
+#comando-> awk '$0 ~ /^#/ {next} {print $1}' $1 | sort
+x=`awk '$0 ~ /^#/ {next} {printf ("%s\n",$1)}' $2 | sort -u`
+x=$x`echo -e '\b'`
+x=$x`awk '$0 ~ /^#/ {next} {printf ("%s\n",$1)}' $1 | sort -u`
+for i in $x
+do
+  echo $i
+done
